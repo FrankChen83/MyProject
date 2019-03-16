1a8a77ef63d84fd3cdc9b5b411f3074a8c9359bf
FrankChen83 <36326121+FrankChen83@users.noreply.github.com>
2019-03-17 00:13:10 +0800

    updated.
    1.0 version complete.

--- MyStart.java
+++ MyStart.java
@@ -1,9 +1,20 @@
 public class MyStart {
-  public MyStart(){};
+  public MyStart(String name,String address){
+    this.name=name;
+    this.address=address;
+  }
+  
+  private String name;
+  private String address;
+  
+  public String makeOne() {
+    return name+address;
+  }
   
   public static void main(String[] str) {
-  
-    System.out.println("MyStart");
+    MyStart ms=new MyStart("王兰花","万花苑23号304号");
+    System.out.println("MyStart..................");
+    System.out.println(ms.makeOne());
   }
   
   public static String theDeveloper() {
